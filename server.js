73070ca5ea8a79801356c7c56db14ab37c42b036
Santiago Harkes <santiagoharkes@gmail.com>
2022-04-21 16:13:53 -0300

    Botcito 2022

--- server.js
+++ server.js
@@ -7,12 +7,277 @@ client.on("ready", () => {
 
 let prefix = process.env.PREFIX;
 
+const docuArray = [
+  {
+    title: "HTML Basics",
+    url: "https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/HTML_basics",
+  },
+  {
+    title: "Head en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Introduction_to_HTML/The_head_metadata_in_HTML",
+  },
+  {
+    title: "Textos en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Introduction_to_HTML/HTML_text_fundamentals",
+  },
+  {
+    title: "Hipervínculos en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Introduction_to_HTML/Creating_hyperlinks",
+  },
+  {
+    title: "Formateo de texto en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Introduction_to_HTML/Advanced_text_formatting",
+  },
+  {
+    title: "Estructurar una pagina en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Introduction_to_HTML/Structuring_a_page_of_content",
+  },
+  {
+    title: "Cosas multimedia en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding",
+  },
+  {
+    title: "Imagenes en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding/Images_in_HTML",
+  },
+  {
+    title: "Audio y video en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding/Video_and_audio_content",
+  },
+  {
+    title: "Imágenes responsive en HTML",
+    url: "https://developer.mozilla.org/es/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images",
+  },
+  {
+    title: "Intro en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS",
+  },
+  {
+    title: "Qué es CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/First_steps/What_is_CSS",
+  },
+  {
+    title: "Cómo se estructura el CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/First_steps/How_CSS_is_structured",
+  },
+  {
+    title: "Cómo funciona el CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/First_steps/How_CSS_works",
+  },
+  {
+    title: "Intro a creando bloques en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks",
+  },
+  {
+    title: "Cascada y Herencia en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Cascade_and_inheritance",
+  },
+  {
+    title: "Selectores en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Selectors",
+  },
+  {
+    title: "Modelo de caja o Box Model en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/The_box_model",
+  },
+  {
+    title: "Fondos y Bordes en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Backgrounds_and_borders",
+  },
+  {
+    title: "Direcciones de texto en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Handling_different_text_directions",
+  },
+  {
+    title: "Overflow en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Overflowing_content",
+  },
+  {
+    title: "Valores y unidades en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Values_and_units",
+  },
+  {
+    title: "Dimensionar elementos en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Sizing_items_in_CSS",
+  },
+  {
+    title: "Imágenes en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Images_media_form_elements",
+  },
+  {
+    title: "Cómo estilar tablas en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Building_blocks/Styling_tables",
+  },
+  {
+    title: "Estilando el texto en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Styling_text",
+  },
+  {
+    title: "Textos y fuentes en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Styling_text/Fundamentals",
+  },
+  {
+    title: "Aplicando estilo a listas",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Styling_text/Styling_lists",
+  },
+  {
+    title: "Estilar los enlaces",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Styling_text/Styling_links",
+  },
+  {
+    title: "Fuentes web en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/Styling_text/Web_fonts",
+  },
+  {
+    title: "Introducción al Diseño en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/CSS_layout/Introduction",
+  },
+  {
+    title: "Normal Flow en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/CSS_layout/Normal_Flow",
+  },
+  {
+    title: "Flexbox en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/CSS_layout/Flexbox",
+  },
+  {
+    title: "Grid en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/CSS_layout/Grids",
+  },
+  {
+    title: "Positions en CSS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Positioning",
+  },
+  {
+    title: "Responsive Design en CSS",
+    url: "https://developer.mozilla.org/es/docs/Learn/CSS/CSS_layout/Responsive_Design",
+  },
+  {
+    title: "Media Queries para NOOOOOOOOOBS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Media_queries",
+  },
+  {
+    title: "Qué es JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript",
+  },
+  {
+    title: "Variables en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables",
+  },
+  {
+    title: "Matemática básica en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math",
+  },
+  {
+    title: "Strings en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Strings",
+  },
+  {
+    title: "Métodos de Strings en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Useful_string_methods",
+  },
+  {
+    title: "Arrays en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays",
+  },
+  {
+    title: "Condicionales en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals",
+  },
+  {
+    title: "Loops o Bucles en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code",
+  },
+  {
+    title: "Funciones en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Functions",
+  },
+  {
+    title: "Lo que devuelve una función (return) en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Return_values",
+  },
+  {
+    title: "Introducción a Eventos en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events",
+  },
+  {
+    title: "Lo básico de Objetos en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics",
+  },
+  {
+    title: "Prototipos en Objetos de JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes",
+  },
+  {
+    title: "Programación orientada a objetos (POO) en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_programming",
+  },
+  {
+    title: "Classes en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Classes_in_JavaScript",
+  },
+  {
+    title: "Asincronismo en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous",
+  },
+  {
+    title: "Intro al asincronismo en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing",
+  },
+  {
+    title: "Cómo usar promesas en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Promises",
+  },
+  {
+    title: "Introducción a las WEB APIs",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction",
+  },
+  {
+    title: "Fetch en JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data",
+  },
+  {
+    title: "Cómo se estructura un Form",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Forms/How_to_structure_a_web_form",
+  },
+  {
+    title: "Types de los inputs en HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types",
+  },
+  {
+    title: "Estilando Forms en CSS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Forms/Styling_web_forms",
+  },
+  {
+    title: "Validación de inputs en HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation",
+  },
+  {
+    title: "Qué es la Accesibilidad",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Accessibility/What_is_accessibility",
+  },
+  {
+    title: "Principios de Accesibilidad en HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML",
+  },
+  {
+    title: "Principios de Accesibilidad en CSS y JS",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Accessibility/CSS_and_JavaScript",
+  },
+];
+
+const randomNumber = Math.floor(Math.random() * docuArray.length + 1);
+const randomDocu = docuArray[randomNumber];
+
 client.on("message", async (message) => {
   // if (message.author.bot) return;
 
   if (
     message.content.toUpperCase().includes("puto".toUpperCase()) ||
     message.content.toUpperCase().includes(" puta".toUpperCase()) ||
+    message.content.toUpperCase().includes("hdp".toUpperCase()) ||
+    message.content.toUpperCase().includes("p*ta".toUpperCase()) ||
+    message.content.toUpperCase().includes("put*".toUpperCase()) ||
     message.content.toUpperCase().includes("hdp".toUpperCase()) ||
     message.content.toUpperCase().includes("trolo".toUpperCase()) ||
     message.content.toUpperCase().includes("culia".toUpperCase()) ||
@@ -40,12 +305,6 @@ client.on("message", async (message) => {
     message.reply("SANTI NO ROMPIÓ NADA, CALMATE");
   }
 
-  // if (message.content.toUpperCase().includes("luis".toUpperCase())) {
-  //   if (message.author != "829051360183976046") {
-  //     message.channel.send("Luissss grande mi pana! 💪🏼");
-  //   }
-  // }
-
   if (message.content.toUpperCase().includes("php".toUpperCase())) {
     if (message.author != "829051360183976046") {
       message.reply("Está prohibido hablar de PHP, primer aviso (?)");
@@ -58,25 +317,25 @@ client.on("message", async (message) => {
     }
   }
 
-  if (message.content.toUpperCase().includes("caro".toUpperCase())) {
-    if (message.author != "829051360183976046") {
-      message.channel.send("CAROOOO TE AMOOOOOOO ❤️");
-    }
-  }
+  // if (message.content.toUpperCase().includes("caro".toUpperCase())) {
+  //   if (message.author != "829051360183976046") {
+  //     message.channel.send("CAROOOO TE AMOOOOOOO ❤️");
+  //   }
+  // }
 
-  if (
-    (message.content.toUpperCase().includes("github".toUpperCase()) ||
-      message.content.toUpperCase().includes("git".toUpperCase())) &&
-    (message.content.toUpperCase().includes("cambiar".toUpperCase()) ||
-      message.content.toUpperCase().includes("cambio".toUpperCase())) &&
-    message.content.toUpperCase().includes("rama".toUpperCase())
-  ) {
-    if (message.author != "829051360183976046") {
-      message.channel.send(
-        "Para cambiar de rama en github tenés que ponerte a estudiar un poco soquete :)"
-      );
-    }
-  }
+  // if (
+  //   (message.content.toUpperCase().includes("github".toUpperCase()) ||
+  //     message.content.toUpperCase().includes("git".toUpperCase())) &&
+  //   (message.content.toUpperCase().includes("cambiar".toUpperCase()) ||
+  //     message.content.toUpperCase().includes("cambio".toUpperCase())) &&
+  //   message.content.toUpperCase().includes("rama".toUpperCase())
+  // ) {
+  //   if (message.author != "829051360183976046") {
+  //     message.channel.send(
+  //       "Para cambiar de rama en github tenés que ponerte a estudiar un poco soquete :)"
+  //     );
+  //   }
+  // }
 
   // if (
   //   message.content.toUpperCase().includes("facu".toUpperCase()) ||
@@ -121,7 +380,7 @@ client.on("message", async (message) => {
     if (message.author != "829051360183976046") {
       //   message.reply("EL CITIOU ES LO MÁS GRANDE ❤️");
       message.reply(
-        "Hola soy Tiburoncin! El más mejor bot de todos y guardaespaldas personal de Santi 🦈 haganme caso o moriré"
+        "Hola soy Tiburoncin! El más mejor bot de todos y guardaespaldas personal de Santi 🦈 haganme caso o no aprenden CSS"
       );
     }
   }
@@ -138,6 +397,25 @@ client.on("message", async (message) => {
     const fetched = await message.channel.messages.fetch({ limit: 100 });
     message.channel.bulkDelete(fetched);
     console.log("Mensajes eliminados");
+  }
+
+  if (message.content.toUpperCase().includes("!adel".toUpperCase())) {
+    const embed = new MessageEmbed()
+      .setTitle("Dijeron DOCUMENTACIÓN?")
+      .setColor("#FF00A2")
+      .setAuthor(
+        "Adel dice:",
+        "https://pbs.twimg.com/profile_images/1468394387000410116/uj5xswRX_400x400.jpg",
+        "https://developer.mozilla.org/es/"
+      )
+      .setDescription(
+        "Te pido por favor que vayas a leer la documentación porque está todo ahí"
+      )
+      .addFields({
+        name: `Querés aprender sobre ${randomDocu.title}?`,
+        value: `Toma, anda a leer la documentación: ${randomDocu.url}`,
+      });
+    message.channel.send(embed);
   }
 
   if (
